071662fc60a1b6e3cf777da504b2286f3f09e20f
Commortel <commortel@gmail.com>
2013-03-26 15:07:17 +0100

    Training class : Launch 1000 Threads with concurrent access

--- TpTempsReel/src/fr/uha/ensisa/tp3/Training.java
+++ TpTempsReel/src/fr/uha/ensisa/tp3/Training.java
@@ -1,14 +1,36 @@
 package fr.uha.ensisa.tp3;
 
-public class Training
+public class Training extends Thread
 {
-	//private ArrayList<Thread> listThread = new ArrayList<Thread>();
-	public static void main(String[] args) 
+    private int m_id;
+	public static int counter = 0;
+	
+	public Training(int id)
 	{
-		for(int i=0; i < 4; i++)
-		{
-			
-		}
+		this.m_id = id;
 	}
-
+	
+	public synchronized void run()
+	{
+		int tmp = counter;
+		tmp = tmp + 1;
+		int tmp2 = tmp;
+		counter = tmp2;
+	}
+	
+	public static void main(String[] args) throws InterruptedException 
+	{
+		Training[] mytn = new Training[1000];
+		
+		for(int i=0; i < mytn.length; i++)
+		{
+			mytn[i] = new Training(i);
+			mytn[i].start();
+		}
+		for(int i=0; i < mytn.length; i++)
+		{
+			mytn[i].join();
+		}
+		System.out.println("Compteur : " + counter);
+	}
 }
